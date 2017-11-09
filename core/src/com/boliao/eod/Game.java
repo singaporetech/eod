5f01542d2b3cf265be4334e8a6d05e1079ea5352
chektien <chek@gamesstudio.org>
2017-11-09 23:04:27 +0800

    updated travis to post to sit-chek

--- core/src/com/boliao/eod/Game.java
+++ core/src/com/boliao/eod/Game.java
@@ -23,7 +23,7 @@ public class Game extends com.badlogic.gdx.Game {
 	@Override
 	public void create () {
         // this is a new comment
-        
+
         // init engines
         RenderEngine.i().init();
         CollisionEngine.i().init();
