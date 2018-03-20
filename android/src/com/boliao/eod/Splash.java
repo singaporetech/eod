01374f759ae86a9741c805769ad46911b6b94fe2
chektien <chek@gamesstudio.org>
2018-03-20 17:46:30 +0800

    done

--- android/src/com/boliao/eod/Splash.java
+++ android/src/com/boliao/eod/Splash.java
@@ -24,7 +24,15 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static java.lang.Thread.sleep;
 
@@ -47,6 +55,15 @@ public class Splash extends AppCompatActivity implements CameraBridgeViewBase.Cv
      * declare native function
      */
     public native String stringFromJNI();
+    public native int convertToGrayscale(long rgbaAddrInput, long rbgaAddrResult);
+    public native void detectFace(String cascadePath, long rgbaAddrInput);
+
+    // TODO NDK
+    // init cv matrix
+    Mat rgbaT, rgbaF;
+    Mat rgbaInput, rgbaOutput;
+    File cascadeFile;
+    String cascadeFileName = "haarcascade_frontalface_alt.xml";
 
     // shared preferences setup
     public final static String PREF_FILENAME = "com.boliao.eod.prefs";
@@ -72,6 +89,37 @@ public class Splash extends AppCompatActivity implements CameraBridgeViewBase.Cv
         camView = findViewById(R.id.camview);
         camView.setVisibility(SurfaceView.VISIBLE);
         camView.setCvCameraViewListener(this);
+
+        // getting permissions for camera
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                Toast.makeText(this, "we need your permission lah deh", Toast.LENGTH_SHORT).show();
+
+                Log.i(TAG, "CAMERA PERMISSION");
+            } else {
+
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},0);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+            Log.i(TAG, "CAMERA PERMISSION GRANTED");
+        }
 
         // TODO NETWORKING
         // getting permissions for location services
@@ -190,15 +238,34 @@ public class Splash extends AppCompatActivity implements CameraBridgeViewBase.Cv
      */
     @Override
     public void onCameraViewStarted(int width, int height) {
-
+        rgbaInput = new Mat(height, width, CvType.CV_8UC4);
+        rgbaOutput = new Mat(height, width, CvType.CV_8UC4);
+        rgbaF = new Mat(height, width, CvType.CV_8UC4);
+        rgbaT = new Mat(width, width, CvType.CV_8UC4);
     }
     @Override
     public void onCameraViewStopped() {
-
+        rgbaInput.release();
+        rgbaOutput.release();
+        rgbaF.release();
+        rgbaT.release();
     }
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return null;
+        rgbaInput = inputFrame.rgba();
+
+        // flip the pixel orientation
+//        Core.transpose(rgba, rgbaT);
+//        Imgproc.resize(rgbaT, rgbaF, rgbaF.size(), 0, 0, 0);
+//        Core.flip(rgbaF, rgba, 1);
+
+        // do convert to grayscale
+//        convertToGrayscale(rgbaInput.getNativeObjAddr(), rgbaOutput.getNativeObjAddr());
+
+        // do face detection
+        detectFace(cascadeFile.getAbsolutePath(), rgbaInput.getNativeObjAddr());
+
+        return rgbaInput;
     }
 
     CameraBridgeViewBase camView;
@@ -226,6 +293,7 @@ public class Splash extends AppCompatActivity implements CameraBridgeViewBase.Cv
         } else {
             Log.d(TAG, "OpenCV library found inside package. Using it!");
             baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            loadCascadeFile();
         }
     }
 
@@ -244,5 +312,28 @@ public class Splash extends AppCompatActivity implements CameraBridgeViewBase.Cv
     protected void onPause() {
         super.onPause();
         disableCam();
+    }
+
+    private void loadCascadeFile() {
+        final InputStream is;
+        FileOutputStream os;
+        try {
+            is = getResources().getAssets().open("data/" + cascadeFileName);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            cascadeFile = new File(cascadeDir, "face_frontal.xml");
+
+            os = new FileOutputStream(cascadeFile);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+
+            is.close();
+            os.close();
+        } catch (IOException e) {
+            Log.i(TAG, "face cascade not found");
+        }
     }
 }
